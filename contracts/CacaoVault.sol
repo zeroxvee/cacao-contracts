e044d2b9cceba0f40f074c5af291e00c5e47a28d
vbazhutin <vladimir.bazhutin@gmail.com>
2023-02-20 22:50:36 -0500

    CacaoVault: utility nft mint, deposit and withdraw functions

--- contracts/CacaoVault.sol
+++ contracts/CacaoVault.sol
@@ -12,17 +12,58 @@ contract CacaoVault is ERC721 {
     constructor(
         string memory _name,
         string memory _symbol
-    ) ERC721(_name, _symbol) {}
+    ) ERC721(_name, _symbol) {
+        _mint(msg.sender, tokenCounter);
+        tokenCounter++;
+    }
 
     address private cacao;
+    uint256 tokenCounter;
 
     // NFT collection address => tokenID => Owner address
-    mapping(address => mapping(uint256 => address)) tokenIdToOwner;
+    mapping(address => mapping(uint256 => Offer)) tokenIdToOffer;
 
     // NFT collection address => tokenID => offerID
     mapping(address => mapping(uint256 => uint256)) tokenToOfferId;
 
+    struct Offer {
+        address nftOwner;
+        uint256 duration;
+    }
+
     function setCacaoAddress(address _cacao) external {
         cacao = _cacao;
+    }
+
+    function depositNft(
+        address _collection,
+        uint256 _tokenId,
+        address _owner,
+        uint256 _duration
+    ) external {
+        address owner = IERC721(_collection).ownerOf(_tokenId);
+        require(msg.sender == owner || msg.sender == cacao, "Not owner");
+        IERC721(_collection).transferFrom(_owner, address(this), _tokenId);
+        Offer memory newOffer = tokenIdToOffer[_collection][_tokenId];
+        newOffer.nftOwner = _owner;
+        newOffer.duration = _duration;
+        _mint(_owner, tokenCounter);
+    }
+
+    function withdrawNft(
+        address collection,
+        uint256 tokenId,
+        address ownerAddress
+    ) external {
+        Offer memory offer = tokenIdToOffer[collection][tokenId];
+        require(
+            msg.sender == offer.nftOwner || msg.sender == cacao,
+            "Not owner"
+        );
+        IERC721(collection).safeTransferFrom(
+            address(this),
+            ownerAddress,
+            tokenId
+        );
     }
 }

c6f41dbaa8e7e269447eb989cef97cc4fb585c0d
vbazhutin <vladimir.bazhutin@gmail.com>
2023-03-21 19:45:20 -0400

    CacaoVault: verification script

--- deploy/01-deploy-cacaoVault.js
+++ deploy/01-deploy-cacaoVault.js
@@ -1,22 +1,21 @@
 const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-
+    const chainId = network.config.chainId
     const delegatorLocal = (
         await ethers.getContract("DelegationRegistry", deployer)
     ).address
     const delegatorGoerli = "0x00000000000076A84feF008CDAbe6409d2FE638B"
     const delegator =
-        network.config.chainId == 31337 || 1337
-            ? delegatorLocal
-            : delegatorGoerli
+        chainId == 31337 || 1337 ? delegatorLocal : delegatorGoerli
 
     const name = "CacaoVaultToken"
     const symbol = "CVT"
     const args = [name, symbol, delegator]
-    const cacao = await deploy("CacaoVault", {
+    const cacaoVault = await deploy("CacaoVault", {
         from: deployer,
         args: args,
         log: true,
@@ -24,6 +23,13 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     })
 
     log("------------------------------------------------------")
+    if (
+        !(chainId === 31337 || chainId === 1337) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        log("Verifying")
+        await verify(cacao.address, args)
+    }
 }
 
 module.exports.tags = ["cacaoVault", "all", "cacaos"]

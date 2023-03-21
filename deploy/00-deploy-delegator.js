82dd4a97fe6faa4677053de3373ea3ea25be5b67
vbazhutin <vladimir.bazhutin@gmail.com>
2023-03-21 19:42:36 -0400

    Delegator: deploy script
    deploy delegator only if current chain is localhost

--- deploy/00-deploy-delegator.js
+++ deploy/00-deploy-delegator.js
@@ -6,14 +6,20 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const chainId = network.config.chainId
 
     const args = []
-    const delegator = await deploy("DelegationRegistry", {
-        from: deployer,
-        args: args,
-        log: true,
-        waitConfirmations: network.config.blockConfirmations || 1,
-    })
-
-    log("------------------------------------------------------")
+    if (chainId === 31337 || chainId === 1337) {
+        console.log("<< Local chain detected, deployed delegator locally >>")
+        const delegator = await deploy("DelegationRegistry", {
+            from: deployer,
+            args: args,
+            log: true,
+            waitConfirmations: network.config.blockConfirmations || 1,
+        })
+    } else {
+        console.log(
+            "<< deploying on chain = %d - using deployed delegator >>",
+            chainId
+        )
+    }
 }
 
 module.exports.tags = ["delegator", "all"]
---
+++ utils/arguments.js
@@ -0,0 +1,5 @@
+const delegatorGoerli = "0x00000000000076A84feF008CDAbe6409d2FE638B"
+const cacaoVault = "0x58a8071B933B125A843FdA109cE1BD475Ec9FfA7"
+const fee = 3
+
+module.exports = [delegatorGoerli, cacaoVault, fee]
